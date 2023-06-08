11972ae04b24c9887a6a945588ca0ffdf720365c
betsiT <betselottesfa9@gmail.com>
2023-06-08 12:16:04 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const social = document.querySelector(".social-btns");
+const share = document.querySelector(".share");
+
+share.addEventListener("click", function () {
+    social.classList.toggle("social-btns1");
+})
+
+
+
+
